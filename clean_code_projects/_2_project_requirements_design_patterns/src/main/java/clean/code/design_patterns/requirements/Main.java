ad5f330f801a0503c43253be94326d36d35db731
Flaviu Ivanciuc <flaviu_ivanciuc@netex.ro>
2021-08-07 12:01:20 +0300

    Design Patterns project uploaded

--- clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Main.java
+++ clean_code_projects/_2_project_requirements_design_patterns/src/main/java/clean/code/design_patterns/requirements/Main.java
@@ -1,8 +1,37 @@
 package clean.code.design_patterns.requirements;
 
-public class Main {
+import clean.code.design_patterns.builder_pattern.Car;
+import clean.code.design_patterns.composite_pattern.Mammal;
+import clean.code.design_patterns.composite_pattern.Reptile;
 
+public class Main {
     public static void main(String[] args) {
-        //TODO implement your design patterns in this package
+
+        Reptile reptile = new Reptile();
+        reptile.reproduce();
+
+        Mammal mammal = new Mammal();
+        mammal.reproduce();
+
+        System.out.println("----------------------------------------------");
+
+        Car audi = new Car.Builder("Audi")
+                .model("A6")
+                .doors(5)
+                .color("Grey")
+                .engineSize("2500 cmc")
+                .yearOfProduction(2016)
+                .build();
+
+        Car volvo = new Car.Builder("Volvo")
+                .model("XC90")
+                .color("White")
+                .doors(5)
+                .engineSize("2000")
+                .yearOfProduction(2020)
+                .build();
+
+        System.out.println(audi);
+        System.out.println(volvo);
     }
 }
